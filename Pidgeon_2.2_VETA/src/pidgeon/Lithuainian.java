46b3b19f8ffcf2973b6bf45b1dbbad3b228f48aa
Calvin Day <cdayvi@gmail.com>
2015-02-09 14:37:11 -0800

    Update Lithuainian.java

--- Pidgeon_2.2_VETA/src/pidgeon/Lithuainian.java
+++ Pidgeon_2.2_VETA/src/pidgeon/Lithuainian.java
@@ -122,7 +122,7 @@ class Lithuanian extends JFrame implements KeyListener {
                     match=true;
                 }
             }
-            if(!match){
+            /*if(!match){
                 if(T1.getText().toLowerCase().equals("verb")||T1.getText().toLowerCase().equals("verbs")){
                     displayArea.append("Regular Conjugation Chart:\n\n"
                         + "Ich\t[stem]+e\n"
@@ -150,7 +150,7 @@ class Lithuanian extends JFrame implements KeyListener {
                         + "They\t\tsie\n"); 
                 match = true;
                 }
-                
+                */
             else{
                 L1.setText("Sorry, I don't know that word.");
             }
